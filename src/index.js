ab547fc684a4f99b04fe065e26ce6fafc7a367ed
Nelson Gonzalez G <nelson.gonzalez.g@hotmail.com>
2022-03-18 22:05:01 -0500

    Adecuacion de la pagina freelancer a la plantilla

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
 import _ from 'lodash';
-import "@css/style.css";
-import "@sass/style.scss";
-import "@img/webpack-logo.png";
-
-
-function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import "@css/normalize.css";
+import "@css/styles.css";
